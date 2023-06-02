0f0efd83ac395ffc969a9be3b4dd4b3233e396fb
nigel robinson <nigelrobinson@MacBook.local>
2023-06-02 16:31:33 +0200

    variables names changed

--- ft_ascii_table.c
+++ ft_ascii_table.c
@@ -6,7 +6,7 @@
 /*   By: nigelrobinson <Nige@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 08:06:29 by nigelrobinson     #+#    #+#             */
-/*   Updated: 2023/06/02 16:16:55 by nigelrobinson    ###   ########.fr       */
+/*   Updated: 2023/06/02 16:30:58 by nigelrobinson    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 #define NBR_COLUMNS 6
 #define TEN 11
 #define STRINGSIZE 103
+#define ASCII_DEL 34
+#define DEL 127
 
 void	ft_ascii_table(void);
-void	ft_printable_ascii(int *counter, int ascii_number);
-void	ft_non_printable_ascii(int *counter, int ascii_number);
+void	ft_printable_ascii(int *column_counter, int ascii_number);
+void	ft_non_printable_ascii(int *column_counter, int ascii_number);
 void	ft_putnbr(int nbr);
 void	ft_putchar(char c);
 
@@ -32,26 +34,26 @@ void	ft_putchar(char c);
 void	ft_ascii_table(void)
 {
 	int	ascii_number;
-	int	newline_counter;
+	int	column_counter;
 
 	ascii_number = 1;
-	newline_counter = 1;
+	column_counter = 1;
 	write(1, "\n\nNon - printable caracters of the ascii table\n\n", 49);
 	while (ascii_number < ASCII_PRINTABLE)
 	{
-		ft_non_printable_ascii(&newline_counter, ascii_number);
+		ft_non_printable_ascii(&column_counter, ascii_number);
 		ascii_number++;
-		newline_counter++;
+		column_counter++;
 	}
-	ft_non_printable_ascii(&newline_counter, 34);
+	ft_non_printable_ascii(&column_counter, ASCII_DEL);
 	write(1, "\n\nPrintable caracters of the ascii table\n\n", 42);
-	newline_counter = 1;
+	column_counter = 1;
 	ascii_number = ASCII_PRINTABLE - 1;
 	while (ascii_number < ASCII_NBR_MAX)
 	{
-		ft_printable_ascii(&newline_counter, ascii_number);
+		ft_printable_ascii(&column_counter, ascii_number);
 		ascii_number++;
-		newline_counter++;
+		column_counter++;
 	}
 	ft_putchar('\n');
 }
@@ -60,7 +62,7 @@ void	ft_ascii_table(void)
 *	Prints to screen the printable caracters
 */
 
-void	ft_printable_ascii(int *counter, int ascii_number)
+void	ft_printable_ascii(int *column_counter, int ascii_number)
 {
 	ft_putchar(ascii_number);
 	write(1, " - ", 3);
@@ -69,10 +71,10 @@ void	ft_printable_ascii(int *counter, int ascii_number)
 	if (ascii_number < 100)
 		ft_putchar(' ');
 	write(1, " |  ", 4);
-	if (*counter == NBR_COLUMNS)
+	if (*column_counter == NBR_COLUMNS)
 	{
 		ft_putchar('\n');
-		*counter = 0;
+		*column_counter = 0;
 	}
 }
 
@@ -80,7 +82,7 @@ void	ft_printable_ascii(int *counter, int ascii_number)
 *	Prints to screen the symbols of non printable caracters
 */
 
-void	ft_non_printable_ascii(int *counter, int ascii_number)
+void	ft_non_printable_ascii(int *column_counter, int ascii_number)
 {
 	int		index;
 	char	char_symbol [STRINGSIZE];
@@ -89,11 +91,11 @@ void	ft_non_printable_ascii(int *counter, int ascii_number)
 	while ((index++) < STRINGSIZE)
 		char_symbol[index] = ("  NULSOHSTXETXEOTENQACKBELBS HT LF VT FF CR SO SI \
 DLEDC1DC2DC3DC4NAKSYNETBCANEM SUBESCFS GS RS US    DEL")[index];
-	if (ascii_number == 34)
-		ft_putnbr(127);
+	if (ascii_number == ASCII_DEL)
+		ft_putnbr(DEL);
 	else
 		ft_putnbr((ascii_number - 1));
-	if (ascii_number < 34)
+	if (ascii_number < ASCII_DEL)
 		ft_putchar(' ');
 	ascii_number = ascii_number * 3;
 	if ((ascii_number / 3) < TEN)
@@ -101,10 +103,10 @@ DLEDC1DC2DC3DC4NAKSYNETBCANEM SUBESCFS GS RS US    DEL")[index];
 	write(1, "- ", 2);
 	write(1, &char_symbol[(ascii_number -1)], 3);
 	write(1, " |  ", 4);
-	if (*counter == NBR_COLUMNS)
+	if (*column_counter == NBR_COLUMNS)
 	{
 		ft_putchar('\n');
-		*counter = 0;
+		*column_counter = 0;
 	}
 }
 
