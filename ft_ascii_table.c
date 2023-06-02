bdb6454568286668b7d2e54039c5d4d5b956e864
nigel robinson <nigelrobinson@MacBook.local>
2023-06-02 10:50:35 +0200

    norminette

--- ft_ascii_table.c
+++ ft_ascii_table.c
@@ -6,7 +6,7 @@
 /*   By: nigelrobinson <Nige@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 08:06:29 by nigelrobinson     #+#    #+#             */
-/*   Updated: 2023/06/02 10:28:52 by nigelrobinson    ###   ########.fr       */
+/*   Updated: 2023/06/02 10:49:55 by nigelrobinson    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #define ASCII_PRINTABLE 32
 #define NBR_COLUMNS 6
 #define TEN 11
-
 
 void	ft_putchar(char c)
 {
@@ -37,67 +36,55 @@ void	ft_putnbr(int nbr)
 	ft_putchar((nbr % 10) + '0');
 }
 
-
-void	ft_layout(int *counter, int ascii_number)
+void	ft_layout_printable_char(int *counter, int ascii_number)
 {
-		ft_putchar(ascii_number);
+	ft_putchar(ascii_number);
+	write(1, " - ", 3);
+	ft_putnbr(ascii_number);
+	ft_putchar(' ');
+	if (ascii_number < 100)
 		ft_putchar(' ');
-		ft_putchar('-');
-		ft_putchar(' ');
-		ft_putnbr(ascii_number);
-		ft_putchar(' ');
-		if (ascii_number < 100)
-			ft_putchar(' ');
-		ft_putchar(' ');
-		ft_putchar('|');
-		ft_putchar(' ');
-		ft_putchar(' ');
-
-		if (*counter == NBR_COLUMNS)
-		{
-			ft_putchar('\n');
-			*counter = 0;
-		}
+	write(1, " |  ", 4);
+	if (*counter == NBR_COLUMNS)
+	{
+		ft_putchar('\n');
+		*counter = 0;
+	}
 }
 
 void	ft_non_printable_char(int *counter, int ascii_number)
 {
-	char array[] [33] = {"NUL", "SOH","STX","ETX","EOT","ENQ","ACK","BEL",
-	"BS ", "HT ", "LF ", "VT ", "FF ", "CR ", "SO ", "SI ", "DLE", "DC1", 
-	"DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM ", "SUB", "ESC", 
-	"FS ", "GS ", "RS ", "US ", "SPC", "DEL"};
+	char	array[][33] = {"NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", \
+"BEL", "BS ", "HT ", "LF ", "VT ", "FF ", "CR ", "SO ", "SI ", "DLE", \
+"DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM ", "SUB", \
+"ESC", "FS ", "GS ", "RS ", "US ", "SPC", "DEL" };
 
-	if(ascii_number == 34)
+	if (ascii_number == 34)
 		ft_putnbr(127);
 	else
 	{
-		ft_putnbr((ascii_number-1));
+		ft_putnbr((ascii_number - 1));
 		ft_putchar(' ');
 	}
 	if (ascii_number < TEN)
 		ft_putchar(' ');
 	ft_putchar('-');
 	ft_putchar(' ');
-	write(1, &array[(ascii_number-1)], 3);
-	ft_putchar(' ');
-	ft_putchar('|');
-	ft_putchar(' ');
-	ft_putchar(' ');
+	write(1, &array[(ascii_number - 1)], 3);
+	write(1, " |  ", 4);
 	if (*counter == NBR_COLUMNS)
 	{
 		ft_putchar('\n');
 		*counter = 0;
 	}
-
 }
-
 
 void	ft_ascii_table(void)
 {
 	int	ascii_number;
 	int	newline_counter;
 	int	count;
-	
+
 	ascii_number = ASCII_PRINTABLE;
 	newline_counter = 1;
 	count = 1;
@@ -114,11 +101,9 @@ void	ft_ascii_table(void)
 	newline_counter = 1;
 	while (ascii_number < ASCII_NBR_MAX)
 	{
-		ft_layout(&newline_counter, ascii_number);
+		ft_layout_printable_char(&newline_counter, ascii_number);
 		ascii_number++;
 		newline_counter++;
 	}
 	ft_putchar('\n');
 }
-
-
